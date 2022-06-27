c913bce38348eb60f51a654720f91685e4241ae8
mikeku1116 <mikekuforgit@gmail.com>
2022-06-27 12:08:45 +0800

    7.4自動化下載網頁CSV檔案資料

---
+++ csv_scraper.py
@@ -0,0 +1,16 @@
+import requests
+import os
+
+
+stocks = ['2330', '6183']
+
+for stock in stocks:
+
+    response = requests.get(
+        f'https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=csv&date=20220627&stockNo={stock}')
+
+    if not os.path.exists('csv'):
+        os.mkdir('csv')
+
+    with open(f'csv\\{stock}.csv', 'wb') as file:
+        file.write(response.content)
